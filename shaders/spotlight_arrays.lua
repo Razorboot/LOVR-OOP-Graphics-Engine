8acfe1c8362f12c560f9b505d14a4b3269081cf2
Razorboot <Susumo211@gmail.com>
2022-12-31 00:09:51 -0800

    Deprecated for oldMain
    This is the old version of the forward renderer.
    This is compatible with oldMain.

--- shaders/spotlight_arrays.lua
+++ shaders/spotlight_arrays.lua
@@ -69,6 +69,11 @@ spotlightShader = lovr.graphics.newShader([[
     float acneBias = 0.025;
 
     // Helper Functions
+    float sampleShadowMap(int lightIndex, vec2 coords, float compare)
+    {
+        return step(compare, getPixel(depthBuffers, coords.xy, lightIndex).r);
+    }
+
     vec4 getShading(int lightIndex, vec3 norm, float cutOff, vec3 spotDir, vec4 specularColor, vec3 lightPos, vec4 liteColor, vec4 PositionLight, vec3 PosWorld)
     {
         //diffuse
@@ -84,7 +89,7 @@ spotlightShader = lovr.graphics.newShader([[
 
         // Shadowmapping
         vec2 projCoords = PositionLight.xy / PositionLight.w * 0.5 + 0.5;
-        int pcfIndex = 2;
+        int pcfIndex = 4;
         
         float visibility = 0.0;
         for (int x = -pcfIndex; x <= pcfIndex; x++) {
@@ -99,14 +104,14 @@ spotlightShader = lovr.graphics.newShader([[
                     closestDepth = 0.;
                 float currentDepth = PositionLight.z / PositionLight.w;
                 float bias = max(0.000005 * (1.0 - dot(norm, lightDir)), 0.0000125 );
-                float currentDepth_biased = currentDepth + bias /*0.0000125*/;
+                float currentDepth_biased = currentDepth + bias;
 
                 visibility += (currentDepth_biased < 0.0 || closestDepth < currentDepth_biased) ? 1.0 : 0.0;
             }
         }
 
-        float shadow = visibility/25.0;
-        //float shadow = 1.0;
+        // Final shadow color
+        float shadow = visibility/50.0;
 
         // Color calculation
         vec4 shading = (diffuse + specular) * shadow;
@@ -117,8 +122,6 @@ spotlightShader = lovr.graphics.newShader([[
         float theta = dot(lightDir, normalize(-spotDir));
         float epsilon = innerCutOff - outerCutOff;
         float intensity = clamp((theta - outerCutOff ) / epsilon, 0.0, 1.0);
-
-        //return shading * intensity;
 
         // Attenuation
         float att = clamp(1.0 - length(lightPos - PosWorld)/ranges[lightIndex], 0.0, 1.0);
@@ -160,6 +163,7 @@ spotlightShader = lovr.graphics.newShader([[
         }
 
         // Finalize
+        //return vec4(vec3(cutOffs[3]), 1.0);
         return vec4(diffuseColor.xyz, 1.0) * (ambience + vec4(clamp(finalShading.x, 0.0, 1.0), clamp(finalShading.y, 0.0, 1.0), clamp(finalShading.z, 0.0, 1.0), 1.0) );
     }
 ]], { flags = {vertexTangents = false } })
