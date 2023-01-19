ee90c23197e1a37d9fea8ab12d1836289ed26566
Razorboot <Susumo211@gmail.com>
2023-01-19 01:22:44 -0800

    Added General library for common functions.

--- lovr_graphics_engine/include.lua
+++ lovr_graphics_engine/include.lua
@@ -1,4 +1,7 @@
+--# Point
 local LGE = {}
+
+--# Modules
 LGE.Scene = require 'lovr_graphics_engine.modules.scene'
 LGE.Node = require 'lovr_graphics_engine.modules.node'
 LGE.Model = require 'lovr_graphics_engine.modules.model'
@@ -6,4 +9,7 @@ LGE.Light = require 'lovr_graphics_engine.modules.light'
 LGE.Body = require 'lovr_graphics_engine.modules.body'
 LGE.Particle = require 'lovr_graphics_engine.modules.particle'
 LGE.Transform = require 'lovr_graphics_engine.modules.transform'
+LGE.General = require 'lovr_graphics_engine.modules.general'
+
+--# Finalize
 return LGE
