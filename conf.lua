fd2b2a41775942013cb7f3a4e1f660be4b1b6f02
Razorboot <Susumo211@gmail.com>
2023-01-19 01:20:23 -0800

    Preparing for managing project settings in the level editor.

--- conf.lua
+++ conf.lua
@@ -1,4 +1,27 @@
+local Serpent = require "lovr_graphics_engine.libs.serpent"
+
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  local lines = {}
+  local lines_str = ""
+  for line in io.lines(file) do 
+      lines[#lines + 1] = line
+      lines_str = lines_str.." "..tostring(line)
+  end
+  return lines, lines_str
+end
+
 function lovr.conf(t)
+  print(lovr.filesystem.getRealDirectory("main.lua").."/user_settings.lua")
+  local lines_t, lines_str = lines_from(lovr.filesystem.getRealDirectory("main.lua").."/user_settings.lua")
+  if #lines_t <= 0 then error("Save file either does not exist, is not a Lua file, or is empty.") return false end
+
   -- Set the project version and identity
   t.version = '0.16.0'
   t.identity = 'default'
@@ -42,8 +65,13 @@ function lovr.conf(t)
   t.math.globals = true
 
   -- Configure the desktop window
-  t.window.width = 900
-  t.window.height = 900
+  if lines_str and lines_str then
+    t.window.width = lines_str.width
+    t.window.height = lines_str.height
+  else
+    t.window.width = 900
+    t.window.height = 900
+  end
   t.window.fullscreen = false
   t.window.title = 'LÖVR'
   t.window.icon = nil
