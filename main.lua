f8a116882fc2aa73aa399f6b8dae8ab245121360
Razorboot <Susumo211@gmail.com>
2023-01-19 01:25:14 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,7 +12,6 @@ local groundBody
 local tvBody
 local testParticle
 
-
 --# Misc Variables
 local assets = 'assets/'
 
@@ -78,10 +77,16 @@ function lovr.load()
 
     -- Create a new collider and set it as a kinematic, meaning it's basically a solid collider
     groundModel:setScale(lovr.math.vec3(20, 1, 20))
-    groundBody = LGE.Body({scene = mainScene, parent = testNode, collider_type = "box", use_dimensions = true, model = groundModel.modelInstance, scale = groundModel.localTransform.scale})
+    groundBody = LGE.Body({
+        scene = mainScene, 
+        parent = testNode, 
+        collider_type = "box", 
+        use_dimensions = true, 
+        model = groundModel.modelInstance, 
+        scale = groundModel.localTransform.scale}
+    )
     groundBody:setKinematic(true)
-    groundBody:setLocalRotation(lovr.math.vec3(0, 0, 0, 0))
-    groundBody:setLocalPosition(lovr.math.vec3(0, -3, 0))
+    groundBody:setLocalPosition(0, -3, 0)
     groundModel:setParent(groundBody)
 
     -- Set the local transform back to (0, 0, 0) so the collisions don't appear strange
@@ -103,32 +108,21 @@ function lovr.load()
 end
 
 function lovr.update(dt)
-    -- Simple update for core scene stuff ^^
-    mainScene:update(dt)
-
     -- Move the light cause why not
-    testLight:setLocalPosition(lovr.math.vec3(0, 1 + math.sin(mainScene.timer*4)*0.25, 0))
-    testLight:setLocalRotation(lovr.math.vec4(math.rad(90), 1, 0, 0))
+    testLight:setLocalPosition(lovr.math.vec3(math.sin(mainScene.timer), 1, -2))
+    testLight:lookAt(testModel.globalTransform.position)
 
-    --groundBody:setGlobalRotation(lovr.math.quat(math.rad(25), 1, 0, math.sin(-mainScene.timer)))
-
-    -- Move the entire node around - can you guess what will happen to all of the child Nodes?
-    --testNode:setGlobalRotation(lovr.math.quat(math.rad(25), 1, 0, math.sin(-mainScene.timer)))
-    --testNode:setGlobalPosition(lovr.math.vec3(math.sin(mainScene.timer*4) + 10, testNode.globalTransform.position.y, testNode.globalTransform.position.z))
+    --[[ Move the entire node around - can you guess what will happen to all of the child Nodes?
+    testNode:setGlobalRotation(lovr.math.quat(math.rad(25), 1, 0, math.sin(-mainScene.timer)))
+    testNode:setGlobalPosition(lovr.math.vec3(math.sin(mainScene.timer*4) + 10, testNode.globalTransform.position.y, testNode.globalTransform.position.z))]]
 
     local scaleFactor = 20-- + math.sin(mainScene.timer)
     --groundModel.tileScale:set(scaleFactor, scaleFactor, scaleFactor)
     --groundBody:setGlobalPosition(lovr.math.vec3(math.sin(mainScene.timer), -3, 0))
     --groundModel:setScale(lovr.math.vec3(scaleFactor, 1, scaleFactor))
 
-    -- Update the transformation of all bodies in the scene
-    mainScene:updateBodies()
-    -- Update the transformation of all models in the scene
-    mainScene:updateModels()
-    -- Update the shadowmap depth buffers and transformation of all lights in the scene
-    mainScene:updateLights()
-    -- Update particle physics
-    mainScene:updateParticles(dt)
+    -- Simple update for core scene stuff ^^
+    mainScene:update(dt)
 end
 
 function lovr.draw(pass)
@@ -141,7 +135,6 @@ function lovr.draw(pass)
     return mainScene:drawFull(pass)
 end
 
-local angleNum = 30
 function lovr.keypressed(key)
     -- Disable and enable shadows
     if key == 'f' then
@@ -149,12 +142,10 @@ function lovr.keypressed(key)
     end
     -- Increase or decrease the light angle
     if key == 'o' then
-        angleNum = angleNum + 1
-        testLight:setAngle(angleNum)
+        testLight:setAngle(testLight:getAngle() + 1)
     end
     if key == 'p' then
-        angleNum = angleNum - 1
-        testLight:setAngle(angleNum)
+        testLight:setAngle(testLight:getAngle() - 1)
     end
     if key == 'k' then
         testParticle.hasShadowCastings = not testParticle.hasShadowCastings
@@ -162,12 +153,14 @@ function lovr.keypressed(key)
 
     -- Save the scene to a file!
     if key == 'x' then
-        mainScene:saveToFile("myTestScene")
+        mainScene:saveToFile("scenes/demo_scene")
     end
 
     -- Create a new collider out of testNode
     if key == "c" then
-        if not getmetatable(tvBody) then
+        groundBody:setKinematic(not groundBody.collider:isKinematic())
+
+        --[[if not getmetatable(tvBody) then
             tvBody = LGE.Body({scene = mainScene, parent = testNode, collider_type = "box", use_dimensions = true, model = testModel.modelInstance, scale = testModel.globalTransform.scale})
             tvBody:setGlobalTransformMatrix(testModel.globalTransform.matrix)
             if getmetatable(testModel) then
@@ -176,6 +169,6 @@ function lovr.keypressed(key)
         else
             testModel:setParent(testNode)
             tvBody:destroy()
-        end
+        end]]
     end
 end
